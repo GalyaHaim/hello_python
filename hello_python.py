751b30df9b4cf7d29af2e753bfb3f45ac5d66ce4
Galya Haim <galya.haim@mail.huji.ac.il>
2025-02-23 12:12:17 +1000

    learned to work with sci mode

--- hello_python.py
+++ hello_python.py
@@ -1,1 +1,21 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+x = np.linspace(-2, 2, 400)
+y = x * x
+
+#%
+plt.plot(x, y)
+plt.title('Graph of x*x')
+plt.xlabel('x')
+plt.ylabel('x*x')
+plt.grid(True)
+plt.show()
+
+#%%
+arr = np.arange(0,100, 0.5)
 print('hello world')
+print(f'This is my new numpy array: {arr}')
+# %%
